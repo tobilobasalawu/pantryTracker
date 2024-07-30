773f2c6b56d8c7de7967045fefdca70ea5d1aff1
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-07-30 17:42:57 +0100

    intial files from nodejs

---
+++ pantry-tracker/app/page.js
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+
+const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot']
+
+export default function Home() {
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack width="800px" height="600px">
+
+      </Stack>
+    </Box>
+  );
+}
+
