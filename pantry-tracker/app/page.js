ddb9f77ac578a3ce89d002e000c32d3f74b66af0
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-07-30 18:22:17 +0100

    No progress with the structure

--- pantry-tracker/app/page.js
+++ pantry-tracker/app/page.js
@@ -1,18 +1,29 @@
-import { Box } from '@mui/material';
+import { Box, Stack} from '@mui/material';
 
-const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot']
+const item = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot', 'lettuce', 'kale', 'cucumber']
 
 export default function Home() {
   return (
     <Box 
       width="100vw" 
       height="100vh"
-      display="flex"
+      display={'flex'}
       justifyContent="center"
       alignItems="center"
     >
-      <Stack width="800px" height="600px">
-
+      <Stack width="800px" height="600px" spacing={2} ove>
+        {item.map((i) => (
+          <Box 
+            key={i}
+            width="100vw" 
+            height="100vh"
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            bgcolor={'#f0f0f0'}>
+              {i}
+          </Box>
+        ))}
       </Stack>
     </Box>
   );
